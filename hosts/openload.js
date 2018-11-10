9c39957635bd799bbd087f291cd4fc30686547c1
iris <duymanh90@aol.com>
2018-11-10 14:33:37 +0700

    add source cmovies and host streamango

---
+++ hosts/openload.js
@@ -0,0 +1,12 @@
+
+
+let host = {
+	priority : 1,
+	language : ['en'],
+	base_link : '',
+	embed : embed
+};
+
+host.host = async (embed, callback) => {};
+
+WmrBLZZdx2UN26Sq = host;
---
+++ sources/en/cmovies.js
@@ -0,0 +1,192 @@
+
+let source = {
+	priority: 1,
+	language: ['en'],
+	domains : ['cmovieshd.net'],
+	base_link : 'http://cmovieshd.net/',
+  tv_link: 'https://cmovieshd.net/tv-series/',
+  movie_link: 'https://cmovieshd.net/movie/',
+	search_link : 'https://cmovieshd.net/search/?q='
+};
+
+source.streamdor = async (html, src, olod) => {
+
+  let episodeId = html.match(/.*streamdor.co/video/(\d+)/ig);
+
+  if (!episodeId) return;
+
+  parserEpisode = await client.request('https://embed.streamdor.co/video/' + episodeId[0], 'GET', {}, {'Referer': src});
+  parserEpisode = parserEpisode.match(/JuicyCodes.Run\(([^\)]+)/i);
+  parserEpisode = parserEpisode.replace(/\"\s*\+\s*\"/ig, '');
+  parserEpisode = parserEpisode.replace(/[^A-Za-z0-9+\\/=]/i, '');
+  parserEpisode = base64.decode(parserEpisode);
+  parserEpisode = aes(parserEpisode);
+  
+
+  qual = parserEpisode.match(/label:"(.*?)"/i);
+
+  if (qual) {qual = qual[0];}
+  else {qual = 'SD';}
+
+  let findEmbed = parserEpisode.match(/(https://streamango.com/embed/.*?)/i);
+
+
+  if (findEmbed) {
+
+    return detail = {
+      'source': 'streamango.com', 'quality': qual, 'language': 'en', 'url': findEmbed[0], 
+      'info': '', direct: false, 'debridonly': False
+    };
+
+  }
+
+  if (olod) {
+    return {
+      'source': 'openload.co', 'quality': qual, 'language': 'en', 'url': findEmbed[0], 
+      'info': '', direct: false, 'debridonly': False
+    };
+  }
+  return false;
+
+}
+
+source.source = async (url) => {
+  return;
+};
+source.movie = async (infoMovie, listDirect, getDirect, callback) => {
+  try {
+
+    let movieLink = '';
+    let listLink = [];
+
+
+    let searchLink = source.search_link + infoMovie.title + "+" +infoMovie.year;
+    let parser = await client.request(searchLink, 'GET', {}, {}, false, '', '', '', 'dom');
+
+    if (!parser) return;
+
+    let listItem = parser('a.ml-mask');
+
+
+    listItem.each(function() {
+
+      let title = parser(this).attr('title');
+
+      if (title.toLowerCase() == infoMovie.title.toLowerCase() || title.toLowerCase() == infoMovie.title.toLowerCase() + " " + infoMovie.year) {
+        movieLink = parse(this).href;
+        break;
+      }
+    });
+
+    if (movieLink == '') return;
+
+    parser = await client.request(movieLink+"watch", 'GET', {}, {}, false, '' ,'' ,'' ,'dom');
+
+    if (!parser) return;
+
+    let listEps = parser('.btn-eps');
+
+    listEps.each(function() {
+        listLinks.push(parser(this).href);
+    });
+
+    let arrPromise = listLinks.map(async (item) => {
+
+      let parserEmbed = client.request(item, 'GET');
+
+      if (parseEmbed.match(/http.+:\/\/openload\.co\/embed\/.+\"/ig)) {
+
+        let openloadLink = parseEmbed.match(/http.+:\/\/openload.co\/embed\/.+\"/ig); 
+        if (openloadLink) {
+
+          let embed = source.streamdor(trim(openloadLink[0]), item, true);
+          if (embed) {
+
+            getDirect(embed, listDirect, callback);
+          }
+        }
+      } else {
+
+        let embed = source.streamdor(parserEmbed, item, false);
+
+        if(embed) {
+
+          getDirect(embed, listDirect, callback);
+        }
+        
+      }
+    });
+    await Promise.all(arrPromise);
+
+  } catch(e) {
+    return;
+  }
+};
+source.tvshow = async (infoMovie, listDirect, getDirect, callback) => {
+
+  try {
+
+    let tvshowLink = '';
+    let episodeLink = [];
+
+    let searchText = infoMovie.title + ' season ' + infoMovie.season;
+    
+
+    let parser = await client.request(source.search_link+searchText, 'GET', {}, {}, false, '', '', '', 'dom');
+    let listItem = parser('.ml-item');
+
+    listItem.each(function() {
+      tvshowLink = parser(this).href;
+      const title = parser(this).attr('title');
+
+      if (href && title.toLowerCase().replace(/\W+/ig, '') == (infoMovie.title + " - season " + infoMovie.season).toLowerCase().replace(/\W+/ig, '')) {
+        break;
+      }
+    });
+
+    if (!tvshowLink) return;
+
+    parser = client.request(tvshowLink+'watch', 'GET', {}, {}, false, '', '', '', 'dom');
+    let listEps = parser('.btn-eps');
+
+    listEps.each(function() {
+      let eps = parser(this).text;
+      eps = exps.match(/episode *([0-9]+)/i);
+
+      if (eps && eps == infoMovie.episode)  {
+        episodeLink.push(parser(this).href);
+      }
+    });
+
+    let arrPromise = episodeLink.map(async (item) => {
+      parserEmbed = client.request(item, 'GET'); 
+
+      if (parseEmbed.match(/http.+:\/\/openload\.co\/embed\/.+\"/ig)) {
+
+        let openloadLink = parseEmbed.match(/http.+:\/\/openload.co\/embed\/.+\"/ig); 
+        if (openloadLink) {
+
+          let embed = source.streamdor(trim(openloadLink[0]), item, true);
+          if (embed) {
+
+            getDirect(embed, listDirect, callback);
+          }
+        }
+      } else {
+
+        let embed = source.streamdor(parserEmbed, item, false);
+
+        if(embed) {
+
+          getDirect(embed, listDirect, callback);
+        }
+        
+      }
+    });
+
+  } catch(e) {
+    return;
+  }
+};
+
+fhQa4JaHXqYgMdDQ = source;
